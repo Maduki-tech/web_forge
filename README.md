7065fd5c1882d927348a58228b3645de5f3ac3de
Maduki-tech <d.schlueter1011@gmail.com>
2023-12-24 11:04:18 +0100

    added ToC to README.md

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # web_forge
+
+## Implementing
+- [DOM](http://dom.spec.whatwg.org/) 
+- [HTML Parser](https://de.wikipedia.org/wiki/HTML5)
+- [CSS Parser](https://de.wikipedia.org/wiki/Cascading_Style_Sheets)
+- [JavaScript Engine](https://de.wikipedia.org/wiki/JavaScript)
+
--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,7 @@ fn main() {
     let text2 = dom::text("Hello world!2".to_string());
 
     let elem = dom::elem("div".to_string(), dom::AttrMap::new(), vec![text]);
-    let elem2 = dom::elem(
-        "div2".to_string(),
-        dom::AttrMap::new(),
-        vec![elem, text2],
-    );
+    let elem2 = dom::elem("div2".to_string(), dom::AttrMap::new(), vec![elem, text2]);
 
     println!("{:#?}", elem2);
 }
