698f0cfd14937a5fc18e372e860860d3aa5f8a43
Maduki-tech <d.schlueter1011@gmail.com>
2023-12-24 11:01:15 +0100

    added basic DOM Structure

---
+++ src/dom.rs
@@ -0,0 +1,38 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct Node {
+    children: Vec<Node>,
+    node_type: NodeType,
+}
+
+#[derive(Debug)]
+pub enum NodeType {
+    Text(String),
+    Element(ElementData),
+}
+
+#[derive(Debug)]
+pub struct ElementData {
+    tag_name: String,
+    attributes: AttrMap,
+}
+
+pub type AttrMap = HashMap<String, String>;
+
+pub fn text(data: String) -> Node {
+    Node {
+        children: Vec::new(),
+        node_type: NodeType::Text(data),
+    }
+}
+
+pub fn elem(name: String, attrs: AttrMap, children: Vec<Node>) -> Node {
+    Node {
+        children,
+        node_type: NodeType::Element(ElementData {
+            tag_name: name,
+            attributes: attrs,
+        }),
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,15 @@
+pub mod dom;
+
+fn main() {
+    let text = dom::text("Hello world!".to_string());
+    let text2 = dom::text("Hello world!2".to_string());
+
+    let elem = dom::elem("div".to_string(), dom::AttrMap::new(), vec![text]);
+    let elem2 = dom::elem(
+        "div2".to_string(),
+        dom::AttrMap::new(),
+        vec![elem, text2],
+    );
+
+    println!("{:#?}", elem2);
+}
